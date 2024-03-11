9a1911a316ee45dd029a5babad573fe153ace1dd
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-03-11 22:26:08 +0100

    fixing files

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -15,7 +15,7 @@ class AppController {
   static getStats(_req, res) {
     const files = dbClient.nbFiles();
     const users = dbClient.nbUsers();
-    res.status(200).json({ users, files });
+    res.status(200).json({ users: files, files: users });
   }
 }
 
