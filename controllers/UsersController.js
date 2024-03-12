a7ac5978629f6babca02cc7799fc09f59c94c000
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-03-12 20:54:34 +0100

    fixing UserController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,16 +5,16 @@ class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
     if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+      res.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
+      res.status(400).json({ error: 'Missing password' });
     }
 
     const collection = dbClient.db.collection('users');
     const user = await collection.findOne({ email });
     if (user) {
-      return res.status(400).json({ error: 'User already exists' });
+      res.status(400).json({ error: 'Already exist' });
     }
     const hashedPassword = sha1(password);
     const result = await collection.insertOne({ email, password: hashedPassword });
@@ -22,7 +22,7 @@ class UsersController {
       id: result.insertedId,
       email,
     };
-    return res.status(201).json(newUser);
+    res.status(201).json(newUser);
   }
 }
 
